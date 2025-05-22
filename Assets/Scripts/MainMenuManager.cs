9f488b62d167e692227945eabcda8a334e779f23
ViktoriiaPetriv <vika.petriv.05@gmail.com>
2025-05-22 22:45:18 +0300

    Fix menu button and add delete info about player after exit

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Networking;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -36,9 +38,40 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene(gameSceneName);
     }
 
+    
+    private IEnumerator DeletePlayer()
+    {
+        int playerId = SessionData.playerId;
+
+        if (playerId <= 0)
+        {
+            Debug.LogWarning("Invalid player ID.");
+            Application.Quit();
+            yield break;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("player_id", playerId);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://unity-server-sdfo.onrender.com/delete_player.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Failed to delete player: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Player deleted successfully: " + www.downloadHandler.text);
+            }
+        }
+    }
+
     private void OnExitClicked()
     {
         PlayClickSound();
+        StartCoroutine(DeletePlayer());
         Application.Quit();
         Debug.Log("Game exited");
     }
